b4feb883d614c5cab3f68c2713873b1703dff3d6
lsoenning <lukas.soenning@uni-bamberg.de>
2022-11-02 17:53:36 +0100

    restructure website

--- render_html.R
+++ render_html.R
@@ -6,3 +6,10 @@ render("index.Rmd", output_dir = output_dir)
 render("about.Rmd", output_dir = output_dir)
 render("research.Rmd", output_dir = output_dir)
 render("resources.Rmd", output_dir = output_dir)
+
+output_dir <- "../lsoenning.github.io/docs/resources_pages"
+
+render("resources_pages/instructions_box_plot.Rmd", output_dir = output_dir)
+render("resources_pages/instructions_dot_diagram.Rmd", output_dir = output_dir)
+render("resources_pages/instructions_dot_plot.Rmd", output_dir = output_dir)
+render("resources_pages/instructions_scatter_plot.Rmd", output_dir = output_dir)
