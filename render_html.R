db3b2d61a53a0a3fa0bd6b849fdbb76c6294ac49
lsoenning <lukas.soenning@uni-bamberg.de>
2023-01-12 14:29:13 +0100

    test multiplex

--- render_html.R
+++ render_html.R
@@ -1,13 +1,13 @@
 # render html files
 library(rmarkdown)
-output_dir <- "../lsoenning.github.io/docs"
+output_dir <- "../_lsoenning.github.io/docs"
 
 render("index.Rmd", output_dir = output_dir)
 render("about.Rmd", output_dir = output_dir)
 render("research.Rmd", output_dir = output_dir)
 render("resources.Rmd", output_dir = output_dir)
 
-output_dir <- "../lsoenning.github.io/docs/resources_pages"
+output_dir <- "../_lsoenning.github.io/docs/resources_pages"
 
 render("resources_pages/instructions_box_plot.Rmd", output_dir = output_dir)
 render("resources_pages/instructions_dot_diagram.Rmd", output_dir = output_dir)
